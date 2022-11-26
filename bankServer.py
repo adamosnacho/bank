91d5668ad335e4b8f4c3105005152691208ef8c7
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2022-11-26 18:13:15 +0100

    l

--- bankServer.py
+++ bankServer.py
@@ -1,11 +1,8 @@
 import socket
 from threading import Thread
-
-def handleConn(data):
-    print(data)
 quitMsg = 'quit'
 host = socket.gethostname()
-port = 5000  # initiate port no above 1024
+port = 6000  # initiate port no above 1024
 myip = socket.gethostbyname(host)
 server_socket = socket.socket()
 server_socket.bind((host, port))
@@ -16,9 +13,30 @@ while True:
     conn, address = server_socket.accept()
     # receive data stream. it won't accept data packet greater than 1024 bytes
     data = conn.recv(1024).decode()
-    if not data == None:
-        print('from connected user: ' + str(address[0])+' ' + eval(str(data))[0])
-        if eval(data) == 'usrVal':
-            f = open()
+    if not data:
+        break
+    print('from connected user: ' + str(address[0])+' ' + str(eval(str(data))))
+    print(eval(data)[0])
+    if eval(data)[0] == 'usrVal':
+        f = open(eval(data)[1]+'.txt','r')
+        password = eval(str(f.read()))[1]
+        f.close()
+        if password == eval(data)[2]:
+            message = 'userValidated'
+            conn.send(message.encode())
+            print('userValidated')  # send data to the client
+        else:
+            conn.send('userNOTValidated'.encode())
+            print('user not validated')
+    if eval(data)[0] == 'usrNew':
+        try:
+            f = open(eval(data)[1]+'.txt','x')
+            f.close()
+            f = open(eval(data)[1]+'.txt','w')
+            passw = eval(data)[2]
+            dt = [0,passw]
+            f.write(str(dt))
+            f.close()
+        except:pass
         #conn.send(file.encode())  # send data to the client
     conn.close()
--- client.py
+++ client.py
@@ -1,45 +1,37 @@
 import socket
-
+from getch import getche, getch
+host = '192.168.0.63'
+port = 6000  # socket server port number
 def client_program():
-    host = '192.168.0.63'
-    port = 5000  # socket server port number\
-    data = ' '
-
+    client_socket = socket.socket()  # instantiate  # connect to the server
+    client_socket.connect((host, port))
 
 
     message = 'ip of conncted user ' + socket.gethostbyname(socket.gethostname())
-    createUser = input('create new user? (True/False) --> ')
-    if createUser:
+    createUser = input('create new user? (y/n) --> ')
+    if createUser == 'y':
         newUsrName = input('[SETUP] new username --> ')
         newPass = input('[SETUP] new password --> ')
-        file = open(newUsrName + '.txt','w')
-        file.write(newPass)
-        file.close()
+        client_socket.send(str(['usrNew',newUsrName,newPass]).encode())
     username = input('USERNSME --> ')
     password = input('PASSWORD --> ')
-
-    try:
-        userFile = open(username + '.txt','r')
-        if userFile.read() != password:
-            quit('WRONG PASSWORD!')
-        userFile.close()
-    except:
-        quit('NO SUCH USER!')
-
-    client_socket = socket.socket()  # instantiate  # connect to the server
-    client_socket.connect((host, port))
-    message = ['usrVal',username,password]
-    client_socket.send(str(message).encode())  # send message
-    client_socket.close()  # close the connection
-    message = ''
-    while True:
-        client_socket = socket.socket()  # instantiate  # connect to the server
-        client_socket.connect((host, port))
-        print('Received from server: ' + data)  # show in terminal
-        message = input("--> ")  # again take input
-        client_socket.send(str(message).encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        client_socket.close()  # close the connection
+    client_socket.send(str(['usrVal',username,password]).encode())
+    # send message['usrVal',username,password]
+    
+    done = True
+    while done:
+        data = client_socket.recv(1024).decode()
+        if data:
+            print('Received from server: ' + data)
+            if data == 'userNOTValidated':
+                quit('WRONG PASSWORD!')
+        try:
+            inp = getch()
+            print(inp)
+        except:pass
+        if inp == 'command':
+            command = input('[COMMAND] --> ')
+            client_socket.send(str(command).encode())
+    client_socket.close()
 
 client_program()

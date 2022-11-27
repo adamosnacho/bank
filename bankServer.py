566d321df9bf3c5be1b542cf6ca459dc73c3aafb
adamosnacho <116303172+adamosnacho@users.noreply.github.com>
2022-11-27 18:21:47 +0100

    l

--- bankServer.py
+++ bankServer.py
@@ -2,32 +2,33 @@ import socket
 from threading import Thread
 quitMsg = 'quit'
 host = socket.gethostname()
-port = 6000  # initiate port no above 1024
+port = 5000  # initiate port no above 1024
 myip = socket.gethostbyname(host)
 server_socket = socket.socket()
 server_socket.bind((host, port))
 print(myip)
 
 server_socket.listen(2)  # accept new connection
+conn, address = server_socket.accept()
 while True:
-    conn, address = server_socket.accept()
     # receive data stream. it won't accept data packet greater than 1024 bytes
     data = conn.recv(1024).decode()
     if not data:
         break
     print('from connected user: ' + str(address[0])+' ' + str(eval(str(data))))
-    print(eval(data)[0])
     if eval(data)[0] == 'usrVal':
-        f = open(eval(data)[1]+'.txt','r')
-        password = eval(str(f.read()))[1]
-        f.close()
-        if password == eval(data)[2]:
-            message = 'userValidated'
-            conn.send(message.encode())
-            print('userValidated')  # send data to the client
-        else:
-            conn.send('userNOTValidated'.encode())
-            print('user not validated')
+        try:
+            f = open(eval(data)[1]+'.txt','r')
+            password = eval(str(f.read()))[1]
+            f.close()
+            if password == eval(data)[2]:
+                message = 'userValidated'
+                conn.send(message.encode())
+                print('[usrVal] userValidated')  # send data to the client
+            else:
+                conn.send('userNOTValidated'.encode())
+                print('[usrVal] user not validated')
+        except:conn.send('userNOTValidated'.encode())
     if eval(data)[0] == 'usrNew':
         try:
             f = open(eval(data)[1]+'.txt','x')
@@ -37,6 +38,22 @@ while True:
             dt = [0,passw]
             f.write(str(dt))
             f.close()
+            print('[usrNew] created new user')
         except:pass
-        #conn.send(file.encode())  # send data to the client
-    conn.close()
+    if eval(data)[0] == 'admLogin':
+        f = open('adminPassword.txt','r')
+        if f.read() == eval(data)[1]:
+            conn.send('adminGranted'.encode())
+        else:
+            conn.send('adminNotGranted'.encode())
+    if eval(data)[0] == 'bal':
+        try:
+            f = open(eval(data)[1] + '.txt')
+            bal_ = eval(f.read())[0]
+            f.close()
+            conn.send(str(bal_).encode())
+        except:pass
+    if eval(data)[0] == 'trans':
+        pass
+    
+conn.close()
--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 from getch import getche, getch
 host = '192.168.0.63'
-port = 6000  # socket server port number
+port = 5000  # socket server port number
 def client_program():
     client_socket = socket.socket()  # instantiate  # connect to the server
     client_socket.connect((host, port))
@@ -19,19 +19,41 @@ def client_program():
     # send message['usrVal',username,password]
     
     done = True
+    isAdmin = False
     while done:
         data = client_socket.recv(1024).decode()
         if data:
             print('Received from server: ' + data)
             if data == 'userNOTValidated':
-                quit('WRONG PASSWORD!')
+                quit('WRONG PASSWORD / WRONG USERNAME')
+            if data == 'adminGranted':
+                isAdmin = True
+                print('[LOGIN] You are now a administrator')
+            if data == 'adminNotGranted':
+                print('[LOGIN] Wrong Password')
+
         try:
             inp = getch()
-            print(inp)
         except:pass
-        if inp == 'command':
-            command = input('[COMMAND] --> ')
-            client_socket.send(str(command).encode())
+
+        if inp != None and inp != '$':
+            inp_ = input('what to do? --> ')
+            if inp_ == 'trans':
+                address_ = input('to who? --> ')
+                amount_ = input('amount to transfer --> ')
+                client_socket.send(str(['trans',address_,amount_]).encode())
+            if inp_ =='bal':
+                client_socket.send(str(['bal',username]).encode())
+
+
+        if inp == '$':
+            if isAdmin:
+                command = input('[COMMAND] --> ')
+                command = '[' + command + ']'
+                client_socket.send(command.encode())
+            else:
+                passw = input('[LOGIN] Admin password --> ')
+                client_socket.send(str(['admLogin',passw]).encode())
     client_socket.close()
 
 client_program()
